c1e3b17b8c5e2e96b4a301bb3f39b880d095c31b
Hsn <ihasan.azimi@gmail.com>
2024-06-10 16:12:08 +0330

    Circular ProgressBar with percentage implemented

--- app/src/main/java/ir/ha/meproject/ui/fragments/temp1/Temp1Fragment.kt
+++ app/src/main/java/ir/ha/meproject/ui/fragments/temp1/Temp1Fragment.kt
@@ -1,14 +1,27 @@
 package ir.ha.meproject.ui.fragments.temp1
 
+import android.graphics.Color
+import ir.ha.meproject.R
 import ir.ha.meproject.databinding.FragmentTemp1Binding
 import ir.ha.meproject.utility.base.BaseFragment
+import ir.ha.meproject.utility.extensions.singleClick
 
 class Temp1Fragment : BaseFragment<FragmentTemp1Binding>(FragmentTemp1Binding::inflate) {
 
     override fun initializing() {
         super.initializing()
 
-        binding.progress.setProgress(36)
+
+        binding.progress.setProgress(64,true)
+
+
+        binding.add.singleClick {
+            binding.progress.addProgress()
+        }
+
+        binding.low.singleClick {
+            binding.progress.lowProgress()
+        }
 
     }
 
